4b6be15be9822138a73bd7811f1072f777d75ae1
Robert Gazizullin <Maydzuri@gmail.com>
2026-03-21 18:27:27 +0700

    New commit

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, WebDriverException
-
 from browser.browser import Browser
 from utils.logger import Logger
 
